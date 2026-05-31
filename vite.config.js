69bc4861c71a313594176a3b37a8c3372f403e08
Adrien <afauconnet@gmail.com>
2026-05-31 14:21:39 +0200

    update base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/smemlab/",
   plugins: [react()],
   server: { port: 5173, open: false }
 });
